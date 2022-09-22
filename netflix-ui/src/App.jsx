9f3c800e1e052662d2a495951a968cc1c68d4461
Luke Cheseldine <cheeseldine@gmail.com>
2022-09-21 20:12:07 -0400

    add video player

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Netflix from "./pages/Netflix";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Player from "./components/Player";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/player" element={<Player />} />
         <Route exact path="/" element={<Netflix />} />
       </Routes>
     </BrowserRouter>
