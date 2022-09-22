bfc77aefb6ee85ce9a93d390952ada1d7fe0fbf8
Luke Cheseldine <cheeseldine@gmail.com>
2022-09-22 01:24:36 -0400

    add thunks, get movies from api, more redux work

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_KEY = "795944d74fca3575256371c60ff84445";
-export const TMBD_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
